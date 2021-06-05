90a543653c291ebffb208be6fbb37fdb766e46da
gorakhgupta <gorakh4000@gmail.com>
2021-06-05 10:28:46 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {question} from './api';
 import { useEffect, useState } from 'react';
 import Myac from './Myac';
 
+
 function App() 
 
 {
@@ -12,7 +13,7 @@ function App()
 
   return (
 <>
-<h1 style={{marginLeft:'470px'}}>This is API of Me 😄 <br/></h1>
+<h1 className="head" >Interview Question 😄 <br/></h1>
  {
   
   curr.map((ele)=>{
